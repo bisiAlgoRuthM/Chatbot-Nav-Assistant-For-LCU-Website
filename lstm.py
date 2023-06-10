49b1d3e0afad6b17c46baa5953081de4f2664347
bisiAlgoRuthM <bisibeauty@yahoo.com>
2023-06-10 01:30:40 -0500

    create lstm

---
+++ lstm.py
@@ -0,0 +1,82 @@
+import nltk
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import json
+import pickle
+import numpy as np
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM, Dropout
+from tensorflow.keras.optimizers import SGD
+
+# Load and preprocess the data
+data_file = open('intents.json').read()
+intents = json.loads(data_file)
+
+words = []
+classes = []
+documents = []
+ignore_words = ['?', '!']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        # Tokenize each word
+        w = nltk.word_tokenize(pattern)
+        words.extend(w)
+        # Add documents to the corpus
+        documents.append((w, intent['tag']))
+        # Add to the classes list
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+# Lemmatize and lowercase each word and remove duplicates
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
+words = sorted(list(set(words)))
+# Sort classes
+classes = sorted(list(set(classes)))
+
+pickle.dump(words, open('texts.pkl', 'wb'))
+pickle.dump(classes, open('labels.pkl', 'wb'))
+
+# Create training data
+training = []
+output_empty = [0] * len(classes)
+
+for doc in documents:
+    bag = []
+    pattern_words = doc[0]
+    pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
+    for w in words:
+        bag.append(1) if w in pattern_words else bag.append(0)
+    
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+    
+    training.append([bag, output_row])
+
+random.shuffle(training)
+training = np.array(training)
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
+
+# Convert training data to LSTM input format
+train_x = np.reshape(train_x, (len(train_x), len(train_x[0]), 1))
+
+# Build the LSTM model
+model = Sequential()
+model.add(LSTM(128, input_shape=(len(train_x[0]), 1)))
+model.add(Dropout(0.5))
+model.add(Dense(64))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+# Compile model
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+# Train the model
+model.fit(train_x, np.array(train_y), epochs=200, batch_size=5, verbose=1)
+
+# Save the model
+model.save('model.h5')
+print("Model created and trained")
